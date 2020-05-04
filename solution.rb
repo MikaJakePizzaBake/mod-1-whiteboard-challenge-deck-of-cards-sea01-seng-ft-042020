48c5da05fb01286065f3fd9834284923c0af69d2
Trevor Low <tlow94@uw.edu>
2020-05-04 20:28:24 +0000

    Automatically backed up by Learn

--- solution.rb
+++ solution.rb
@@ -1,20 +1,8 @@
 require 'pry'
 class Deck
-  rank = %w('A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K')
+  rank = %w('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
   suit = %w('Hearts', 'Clubs', 'Diamonds', 'Spades')
-  deck = []
-  attr_reader :rank, :suit
-  def initialize(rank:, suit:)
-    @rank = rank
-    @suit = suit
-    deck = []
-  end
-  def choose_card
-
-  end
-
 end
 
-class Card
+  
 
-end
