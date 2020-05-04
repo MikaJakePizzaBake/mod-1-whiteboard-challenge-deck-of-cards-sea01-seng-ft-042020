1ec531cc3ed3f76cc02795e0f17cef55a49eb4e1
Trevor Low <tlow94@uw.edu>
2020-05-04 20:13:31 +0000

    Automatically backed up by Learn

--- solution.rb
+++ solution.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Deck
   rank = %w(A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K)
   suit = %w(Hearts, Clubs, Diamonds, Spades)
@@ -5,6 +6,7 @@ class Deck
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+    binding.pry
   end
   def choose_card
 
