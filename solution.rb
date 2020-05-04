cd7aeab44d2f63d4de7ba865c117e56b13485ba2
Trevor Low <tlow94@uw.edu>
2020-05-04 20:13:00 +0000

    Automatically backed up by Learn

--- solution.rb
+++ solution.rb
@@ -1,5 +1,7 @@
 class Deck
-  attr_accessor :rank, :suit
+  rank = %w(A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K)
+  suit = %w(Hearts, Clubs, Diamonds, Spades)
+  attr_reader :rank, :suit
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
